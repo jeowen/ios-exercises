337adbb1d70024e6e6f75f5b4e9ffea1b8bac49b
jason.owen@va.gov <jason.owen@va.gov>
2016-01-09 10:04:58 -0800

    Completed Assignment 10

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,17 +12,38 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    BOOL areCerealsEqual = string1 == string2;
+    NSString *returnText = @"";
+    if (areCerealsEqual){
+        returnText = @"Cereals should be equal.";
+    }
+    else{
+        returnText = @"Cereals should not be equal.";
+    }
+    return (returnText);
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    BOOL areNumbersEqual = number1 == number2;
+    NSString *returnText = @"";
+    if (areNumbersEqual){
+        returnText = @"Six should be equal to six.";
+    }
+    else{
+        returnText = @"Six should not be equal to seven.";
+    }
+    return (returnText);
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    BOOL isNumber1GreaterThanNumber2 = integer1 > integer2;
+    NSString *returnText = @"";
+    if (isNumber1GreaterThanNumber2){
+        returnText = [NSString stringWithFormat:@"%ld is greater than %ld.", integer1, integer2];
+    }
+    return (returnText);
 }
 
 @end
